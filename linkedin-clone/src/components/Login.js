744e0a1187422ee67279e1cbaec2b41868592b6c
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2023-01-07 17:04:11 +0530

    Login page completed

--- linkedin-clone/src/components/Login.js
+++ linkedin-clone/src/components/Login.js
@@ -13,10 +13,51 @@ function Login() {
           <SignIn>Sign In</SignIn>
         </div>
       </Nav>
-      <Section>section</Section>
+      <Section>
+        <MiddleElement>
+          <h1>Welcome to your professional community </h1>
+          <img src="/images/login-hero.svg" alt="" />
+        </MiddleElement>
+        <Form>
+          <Google>
+            <img src="/images/google.svg" alt="" />
+            Sign In with Google
+          </Google>
+        </Form>
+      </Section>
     </Container>
   );
 }
+const MiddleElement = styled.div`
+  width: 100%;
+  h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+      text-align: center;
+      font-size: 20px;
+      width: 100%;
+      line-height: 2;
+    }
+  }
+  img {
+    width: 700px;
+    height: 700px;
+    position: absolute;
+    bottom: -2px;
+    right: -150px;
+    @media (max-width: 768px) {
+      top: 230px;
+      width: initial;
+      height: initial;
+      position: initial;
+    }
+  }
+`;
 const Container = styled.div`
   padding: 0px;
 `;
@@ -84,6 +125,34 @@ const Section = styled.section`
   @media (max-width: 768px) {
     margin: auto;
     min-height: 0px;
+  }
+`;
+
+const Form = styled.div`
+  margin-top: 100px;
+  width: 408px;
+  @media (max-width: 768px) {
+    margin-top: 20px;
+  }
+`;
+const Google = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  height: 56px;
+  width: 100%;
+  border-radius: 28px;
+  box-shadow: inset 0 0 0 1px rgba(0 0 0 / 60%),
+    inset 0 0 0 2px rgba(0 0 0 / 0%), inset 0 0 0 1px rgba(0 0 0 / 0%);
+  vertical-align: middle;
+  z-index: 0;
+  transition-duration: 167ms;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    background-color: rgba(207, 207, 207, 0.25);
+    color: rgba(0, 0, 0, 0.75);
   }
 `;
 export default Login;
