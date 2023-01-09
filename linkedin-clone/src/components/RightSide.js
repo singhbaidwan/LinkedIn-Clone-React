23459d3c758a778adec2327dc38c375061ed962d
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2023-01-09 20:45:07 +0530

    card present on left side on home page styling done

---
+++ linkedin-clone/src/components/RightSide.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function RightSide() {
+  return <Container>right side</Container>;
+}
+const Container = styled.div`
+  grid-area: rightside;
+`;
+export default RightSide;
